4a71aa32b067fde97b03d9b00da895597bfdbd5e
rupumped <rupumped@gmail.com>
2026-04-21 14:53:11 +0300

    version counter

--- webapp/pages/login.vue
+++ webapp/pages/login.vue
@@ -58,7 +58,7 @@
     </div>
 
     <p class="mt-4 text-center text-2xs text-text-muted">
-      Accounts are provisioned by REX staff. Contact your administrator.
+      Accounts are provisioned by REX staff. Version 0.0.0.
     </p>
   </div>
 </template>
